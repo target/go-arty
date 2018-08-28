e4d803c69f976e1167259667c08728d32a057405
JordanSussman <jordan.sussman@target.com>
2018-08-27 21:11:53 -0500

    another attempt at coverage.out

--- test.sh
+++ test.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 echo "mode: set" > coverage.out
-for Dir in $(find ./* -maxdepth 10 -type d );
-do
-        if ls $Dir/*.go &> /dev/null;
-        then
-            go test -coverprofile=profile.out $Dir
-            if [ -f profile.out ]
-            then
-                cat profile.out | grep -v "mode: set" >> coverage.out
-            fi
-fi
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -coverprofile=profile.out $d
+    if [ -f profile.out ]; then
+        cat profile.out | grep -v "mode: set" >> coverage.out
+        rm profile.out
+    fi
 done
