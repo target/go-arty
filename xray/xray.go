9b15cebe18dde8ba01370a37d7175cdacc6ecafe
Jordan Brockopp <Jordan.Brockopp@target.com>
2018-08-28 10:01:01 -0500

    generate accessors for xray package

--- xray/xray.go
+++ xray/xray.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate go run gen-accessors.go
+
 package xray
 
 // Bool is a helper routine that allocates a new bool value
